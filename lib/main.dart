a27b31d165e769addce1befb2c5715e310adc447
MD SHAKIL HOSSAAIN <mdshakilhossain720@gmail.com>
2025-04-28 12:51:58 +0600

    hive add

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:apitest/myapp.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
-  
-  runApp( ProviderScope(child: MyApp()));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Hive
+  await Hive.initFlutter();
+
+  runApp(ProviderScope(child: MyApp()));
 }
