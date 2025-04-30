7cb3fc23bd2b0776fbd65a6d7dac2cc4cbc11782
MD SHAKIL HOSSAAIN <mdshakilhossain720@gmail.com>
2025-04-30 13:29:10 +0600

    payment getway

---
+++ lib/views/payment_section/payment_section.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaymentSectionScreen extends StatefulWidget {
+  const PaymentSectionScreen({super.key});
+
+  @override
+  State<PaymentSectionScreen> createState() => _PaymentSectionScreenState();
+}
+
+class _PaymentSectionScreenState extends State<PaymentSectionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
