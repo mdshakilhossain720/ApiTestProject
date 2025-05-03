a558d5e61137ec41cc255022d75353e7d0b2d7b7
MD SHAKIL HOSSAAIN <mdshakilhossain720@gmail.com>
2025-05-03 11:56:56 +0600

    pininput

--- lib/views/auth/otp_screem.dart
+++ lib/views/auth/otp_screem.dart
@@ -1,33 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
 
-import 'widgets/pinput.dart';
+class OtpScreen extends StatelessWidget {
+  final TextEditingController _controller = TextEditingController();
 
-class OtpScreen extends StatefulWidget {
-  const OtpScreen({super.key});
+  OtpScreen({super.key});
 
-  @override
-  State<OtpScreen> createState() => _OtpScreenState();
-}
+ 
 
-class _OtpScreenState extends State<OtpScreen> {
-  final TextEditingController pinCodeController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Otp Screen")),
-      body: Column(
-        children: [
-            PinPutWidget(
-                    pinCodeController: pinCodeController,
-                    onCompleted: (pin) {},
-                    validator: (value) {
-                      return null;
-                    },
-                  ),
-
-
-        ],
-      ),
+        appBar: AppBar(
+          title: Text('Pinput Example'),
+        ),
+        body: Center(
+          child: Pinput(
+            length: 6,
+            controller: _controller,
+            onChanged: (value) {
+              // Code to execute when there is a change in the entered values
+            },
+            defaultPinTheme: PinTheme(
+              width: 56,
+              height: 56,
+              textStyle: const TextStyle(
+                fontSize: 20,
+                color: Color.fromRGBO(30, 60, 87, 1),
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color.fromRGBO(234, 239, 243, 1)),
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+        ),
+      
     );
   }
 }
